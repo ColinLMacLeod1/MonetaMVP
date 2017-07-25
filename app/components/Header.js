b413d4915b8c9152e7a053b177ce94964fe77a47
Colin MacLeod <colinlmacleod1@gmail.com>
2017-07-24 21:55:18 -0400

    Added feedback button

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import logo from '../assets/images/logo.png'
-const Header = () => (
-		<div className="header" style={{width:'100vw'}}>
-			<img src={logo}/>
-			<h1>MONETTA</h1>
-		</div>
-	)
+import logo from '../assets/images/logo.png';
+import FlatButton from 'material-ui/FlatButton';
+import Drawer from 'material-ui/Drawer';
 
-export default Header;
+export default class Header extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+      open:false
+		}
+	}
+	render() {
+	return (
+			<div className="header" style={{width:'100vw'}}>
+				<img src={logo}/>
+				<h1>MONETTA</h1>
+				<FlatButton label="Feedback" primary={true} onClick={()=>{this.setState({open: !this.state.open});}} />
+				<Drawer open={this.state.open}>
+        </Drawer>
+			</div>
+		);
+	}
+}
