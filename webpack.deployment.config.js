dd64c0a8a2a3055675dab9fae57bdf97c3dac45a
Colin MacLeod <colinlmacleod1@gmail.com>
2017-08-07 21:54:48 -0400

    changed "npm start" to the deploy omcmand, use "npm dev-start" for dev

--- webpack.deployment.config.js
+++ webpack.deployment.config.js
@@ -30,7 +30,13 @@ const config = {
       compress: {
         warnings: false
       }
-    })
+    }),
+		new webpack.DefinePlugin({
+		  'process.env': {
+		    NODE_ENV: JSON.stringify('production')
+		  }
+		}),
+		new webpack.optimize.UglifyJsPlugin()
 	],
 	node: {
 	  fs: 'empty'
