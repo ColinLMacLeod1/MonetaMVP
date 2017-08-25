95a682afebe9e1982ce479cf077341efdce94269
Colin MacLeod <colinlmacleod1@gmail.com>
2017-08-25 16:36:03 -0400

    added example screenshots for teh help tab

---
+++ app/components/Help/Help1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+
+
+const Help1 = ({onSubmit, onChange, errors, user, toLogin}) => (
+
+);
+
+export default Help1;
---
+++ app/components/Help/Help2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'react-router';
+import Card from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+
+
+const Help1 = ({onSubmit, onChange, errors, user, toLogin}) => (
+
+);
+
+export default Help1;
